c2b314ea03020ac590acfb2f7e38f6e49bd3b80e
pinchukovartur <pinchukovartur@gmail.com>
2017-11-16 13:10:03 +0300

    final gc version

--- save_data/views.py
+++ save_data/views.py
@@ -5,6 +5,7 @@ import mysql.connector
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 import json
+import collections
 
 
 @csrf_exempt
@@ -15,7 +16,7 @@ def save_data(request):
         user_secret_key = request.POST['user_secret_key']
         level_session_id = request.POST['level_session_id']
 
-        query = "INSERT INTO analytic_data_store.tesert_data (key_event, json_data, user_secret_key, level_session_id)" \
+        query = "INSERT INTO analytic_data_store.tester_data (key_event, json_data, user_secret_key, level_session_id)" \
                 " VALUES('" + key + "', '" + data + "', '" + user_secret_key + "', '" + level_session_id + "'); "
         __insert__(query)
 
@@ -58,18 +59,22 @@ def get_list_levels(request):
     for key, value in data.items():
         json_data = json.loads(str(value["json_data"]).replace('"{', '{').replace('}"', '}'))
         levels.add(json_data["m_levelID"])
-
+    if len(levels) == 0:
+        return HttpResponse("Levels Not Found")
     return render(request, 'analytic_data/levels.html', {'levels': levels})
 
 
 def get_level(request):
     # получение даных с бд
+    global events_data, try_count, common_data, complete_count, fail_count, spent_turn_count, spent_second_count, target1_count, target2_count, give_turn_count, give_second_count, event_data, target2sent_count, target1sent_count
     level_name = request.GET['level_name']
 
     data = __get_events_from_db__("startgame")
-
+    # sort
+    #data = collections.OrderedDict(sorted(data.items()))
     # получаем pk всех ивентов, которые содержат level_name
     level_events = dict()
+    event_key = 1
     for key, value in data.items():
         value["event_data"] = json.loads(str(value["json_data"]).replace('"{', '{').replace('}"', '}'))
         value["json_data"] = None
@@ -81,71 +86,133 @@ def get_level(request):
 
                 # добавляем финальный event
                 for key_final_event, value_final_event in final_event.items():
-                    value_final_event["event_data"] = json.loads(str(value_final_event["json_data"]).replace('"{', '{').replace('}"', '}'))
+                    value_final_event["event_data"] = json.loads(
+                        str(value_final_event["json_data"]).replace('"{', '{').replace('}"', '}'))
                     value_final_event["json_data"] = None
                     event_dict = dict()
                     event_dict["startevent"] = value
                     event_dict["endevent"] = value_final_event
                     level_events[key] = event_dict
+                    event_key += 1
 
-
-        data_level = dict()
+        events_data = dict()
         count = 0
+
+        common_data = dict()
+
+        try_count = list()
+        complete_count = list()
+        fail_count = list()
+        spent_turn_count = list()
+        spent_second_count = list()
+        give_turn_count = list()
+        give_second_count = list()
+        target1sent_count = list()
+        target1_count = list()
+        target2_count = list()
+        target2sent_count = list()
+        level_events = collections.OrderedDict(sorted(level_events.items()))
         for key, value in level_events.items():
-            return_dict = dict()
-            return_dict["starttime"] = value["startevent"]["event_datetime"]
-            return_dict["endtime"] = value["endevent"]["event_datetime"]
+            event_data = dict()
+            event_data["starttime"] = value["startevent"]["event_datetime"]
+            event_data["endtime"] = value["endevent"]["event_datetime"]
 
             # try
             count += 1
-            return_dict["try"] = count
+            event_data["try"] = count
+            try_count.append(count)
 
             # complete/fail
-            return_dict["result"] = value["endevent"]["key_event"]
+            if value["endevent"]["key_event"] == "completegame":
+                event_data["completegame"] = 1
+                complete_count.append(1)
+            elif value["endevent"]["key_event"] == "failgame":
+                event_data["failgame"] = 1
+                fail_count.append(1)
 
             # targets
-            return_dict["wasturns"] = value["endevent"]["event_data"]["m_turns"]
-            return_dict["wasseconds"] = value["endevent"]["event_data"]["m_seconds"]
+            # spent
+            spent_second = int(value["endevent"]["event_data"]["m_seconds"])
+            if spent_second > 0:
+                event_data["wasseconds"] = spent_second
+                spent_second_count.append(spent_second)
+            spent_turn = int(value["endevent"]["event_data"]["m_turns"])
+            if spent_turn > 0:
+                event_data["wasturns"] = spent_turn
+                spent_turn_count.append(spent_turn)
+            # given
+            give_second = int(value["startevent"]["event_data"]["m_seconds"])
+            if give_second > 0:
+                event_data["seconds"] = give_second
+                give_second_count.append(give_second)
+            give_turn = int(value["startevent"]["event_data"]["m_turns"])
+            if give_turn > 0:
+                event_data["turns"] = give_turn
+                give_turn_count.append(give_turn)
 
-            return_dict["turns"] = value["startevent"]["event_data"]["m_turns"]
-            return_dict["seconds"] = value["startevent"]["event_data"]["m_seconds"]
+            target1 = int(value["endevent"]["event_data"]["m_firstTarget"]["m_required"])
+            if target1 > 0:
+                # m_required - надо было собрать,   m_remainder - было собрано
+                target1_count.append(target1)
+                event_data["target1"] = str(target1)
+                event_data["target1type"] = value["endevent"]["event_data"]["m_firstTarget"]["m_type"]
+            target2 = int(value["endevent"]["event_data"]["m_secondTarget"]["m_required"])
+            if target2 > 0:
+                # m_required - надо было собрать,   m_remainder - было собрано
+                target2_count.append(target2)
+                event_data["target2"] = str(target2)
+                event_data["target2type"] = value["endevent"]["event_data"]["m_secondTarget"]["m_type"]
 
-            if value["endevent"]["event_data"]["m_firstTarget"]["m_type"] != "":
-                return_dict["target1"] = value["endevent"]["event_data"]["m_firstTarget"]
-            if value["endevent"]["event_data"]["m_secondTarget"]["m_type"] != "":
-                return_dict["target2"] = value["endevent"]["event_data"]["m_secondTarget"]
+            target1sent = int(value["endevent"]["event_data"]["m_firstTarget"]["m_remainder"])
+            if target1sent > 0:
+                target1sent_count.append(target1sent)
+                event_data["target1sent"] = str(target1sent)
+            target2sent = int(value["endevent"]["event_data"]["m_secondTarget"]["m_remainder"])
+            print(value["endevent"]["event_data"]["m_secondTarget"])
+            if target2sent > 0:
+                target2sent_count.append(target2sent)
+                event_data["target2sent"] = str(target2sent)
 
-            data_level[key] = return_dict
-    #print(level_events)
-    return render(request, 'analytic_data/level.html', {'events': data_level})
-"""
-            # try
-            count += 1
-            return_dict["try"] = count
+            events_data[len(events_data) + 1] = event_data
 
-            # complete/fail
-            return_dict["result"] = value["endevent"]["key_event"]
+    common_data["complete"] = sum(complete_count)
+    common_data["fail"] = sum(fail_count)
+    if len(spent_turn_count) > 0:
+        common_data["spent_turn"] = str(float(sum(spent_turn_count) / len(spent_turn_count))) + " +- " \
+                                + str((max(spent_turn_count) - min(spent_turn_count)) / 2)
+    if len(spent_second_count) > 0:
+        common_data["spent_second"] = str(float(sum(spent_second_count) / len(spent_second_count))) + " +- " \
+                                + str((max(spent_second_count) - min(spent_second_count)) / 2)
+    if len(target1_count) > 0:
+        common_data["target1"] = str(float(sum(target1_count) / len(target1_count))) + " +- " \
+                                + str((max(target1_count) - min(target1_count)) / 2)
+    if len(target2_count) > 0:
+        common_data["target2"] = str(float(sum(target2_count) / len(target2_count))) + " +- " \
+                                + str((max(target2_count) - min(target2_count)) / 2)
+    if len(target1sent_count) > 0:
+        common_data["target1sent"] = str(float(sum(target1sent_count) / len(target1sent_count))) + " +- " \
+                                + str((max(target1sent_count) - min(target1sent_count)) / 2)
+    if len(target2sent_count) > 0:
+        common_data["target2sent"] = str(float(sum(target2sent_count) / len(target2sent_count))) + " +- " \
+                                + str((max(target2sent_count) - min(target2sent_count)) / 2)
+    if len(spent_turn_count) > 0:
+        common_data["spent_turn"] = str(float(sum(spent_turn_count) / len(spent_turn_count))) + " +- " \
+                                + str((max(spent_turn_count) - min(spent_turn_count)) / 2)
+    if len(give_turn_count) > 0:
+        common_data["give_turn"] = str(float(sum(give_turn_count) / len(give_turn_count))) + " +- " \
+                                + str((max(give_turn_count) - min(give_turn_count)) / 2)
+    if len(give_second_count) > 0:
+        common_data["give_second"] = str(float(sum(give_second_count) / len(give_second_count))) + " +- " \
+                                + str((max(give_second_count) - min(give_second_count)) / 2)
 
-            # targets
-            return_dict["wasturns"] = value["endevent"]["event_data"]["m_turns"]
-            return_dict["wasseconds"] = value["endevent"]["event_data"]["m_seconds"]
-
-            return_dict["turns"] = value["startevent"]["event_data"]["m_turns"]
-            return_dict["seconds"] = value["startevent"]["event_data"]["m_seconds"]
-            if value["endevent"]["event_data"]["m_firstTarget"]["m_type"] != "":
-                return_dict["target1"] = value["endevent"]["event_data"]["m_firstTarget"]
-            if value["endevent"]["event_data"]["m_secondTarget"]["m_type"] != "":
-                return_dict["target2"] = value["endevent"]["event_data"]["m_secondTarget"]
-"""
-
-
-
-
+    return render(request, 'analytic_data/level.html', {'events': events_data, "common_data": common_data,
+                                                        "target1": event_data["target1type"],
+                                                        "target2": event_data["target2type"]})
 
 
 def __get_events_from_db__(key_event):
     game_query = "SELECT id, key_event, json_data, user_secret_key, level_session_id, event_datetime " \
-                 "FROM analytic_data_store.tester_data WHERE key_event = '" + key_event + "';"
+                 "FROM analytic_data_store.tester_data WHERE key_event = '" + key_event + "' order by event_datetime;"
     cnx = mysql.connector.connect(user='root', database='analytic_data_store', password="root")
     cursor = cnx.cursor()
 
@@ -166,7 +233,7 @@ def __get_events_from_db__(key_event):
 def __get_final_event__(session_key):
     game_query = "SELECT id, key_event, json_data, user_secret_key, level_session_id, event_datetime" \
                  " FROM analytic_data_store.tester_data" \
-                 " WHERE key_event != 'startgame' and level_session_id = '" + session_key + "';"
+                 " WHERE key_event != 'startgame' and level_session_id = '" + session_key + "' order by event_datetime;"
     cnx = mysql.connector.connect(user='root', database='analytic_data_store', password="root")
     cursor = cnx.cursor()
 
@@ -186,7 +253,3 @@ def __get_final_event__(session_key):
         return final_event
     elif len(final_event) > 1:
         raise NameError("> 1 level finish, check validate data base")
-
-
-def __get_analytic_data(level_events):
-    pass
